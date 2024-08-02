62487c712e78222c8c87eb7bfadeb72185efd2f7
dev-ashanaur <devashanaur009@gmail.com>
2024-08-02 20:45:51 +0600

    change mongo url

--- src/app/services/mongo.js
+++ src/app/services/mongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectDB () {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI);
     console.log(`Mongodb connected to ${conn.connection.host} `);
   } catch (error) {
     console.log(error);
